1b6cdfa90cabc3ce7aa56ec0f8931ca9234cc6bb
mzdesa <52471105+mzdesa@users.noreply.github.com>
2021-02-17 23:16:41 -0800

    Changed pin definition

--- lab3/blink.py
+++ lab3/blink.py
@@ -3,13 +3,13 @@ from machine import Pin
 import time
 
 #initialize the GPIO pin number 14, store it in the variable pin_14
-pin_14 = Pin(GPIO14, mode = Pin.OUT)
+pin_14 = Pin(14, mode = Pin.OUT)
 #cycle the square wave 300 times
 i = 0 #counter variable
 t = 0.2 #time to hold an output for
 while i<300:
-    led(1) #hold at a high value for 0.2s
+    pin_14(1) #hold at a high value for 0.2s
     time.sleep(t)
-    led(0)
+    pin_14(0)
     time.sleep(t) #hold at a low value for 0.2s
     i+=1
