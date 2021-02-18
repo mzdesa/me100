1da63dcffb68a9efedd8f9da5ac59d052d680182
mzdesa <52471105+mzdesa@users.noreply.github.com>
2021-02-17 23:08:01 -0800

    Added lab 3 files

---
+++ lab3/blink.py
@@ -0,0 +1,15 @@
+from board import LED
+from machine import Pin
+import time
+
+#initialize the GPIO pin number 14, store it in the variable pin_14
+pin_14 = Pin(GPIO14, mode = Pin.OUT)
+#cycle the square wave 300 times
+i = 0 #counter variable
+t = 0.2 #time to hold an output for
+while i<300:
+    led(1) #hold at a high value for 0.2s
+    time.sleep(t)
+    led(0)
+    time.sleep(t) #hold at a low value for 0.2s
+    i+=1
